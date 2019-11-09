b2eff3b666a58ffdd0ca10b59ee8ca2703354157
Soma Zsják <zsjaks@gmail.com>
2019-11-09 13:47:02 +0100

    Updated publish.bat.

--- publish.bat
+++ publish.bat
@@ -13,16 +13,19 @@ set docsbase=docs
 set distbase=%webappbase%\bin\Release\netstandard2.0\publish\aoc2019.WebApp\dist
 set githuburl=https://sanraith.github.io/aoc2019/
 
+echo Publishing %webappbase%
 cd %webappbase%
 dotnet publish -c Release
 cd ..
 
-for /f "tokens=*" %%G in ('dir /b /a:d "%docsbase%"') do (
-	echo Deleting: %docsbase%\%%G
-	rmdir /S /Q %docsbase%\%%G
-)
+echo Deleting %docsbase%\
+rmdir /S /Q %docsbase%
+
+echo Creating %docsbase%\
+mkdir %docsbase%
 
 echo Copying %distbase% -^> %docsbase%\
 xcopy /E /H /R /Q /Y %distbase% %docsbase%\
 
+echo Replacing base url in %docsbase%\index.html to %githuburl%
 powershell -Command "(gc %docsbase%\index.html) -replace '<base href=\"/\" />', '<base href=\"%githuburl%\" />' | Out-File %docsbase%\index.html"
