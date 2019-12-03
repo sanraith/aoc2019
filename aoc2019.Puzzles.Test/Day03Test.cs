ffd708f26f4acfc37d48d16b4708d9982f25c39d
sanraith <zsjaks@gmail.com>
2019-12-03 16:16:09 +0100

    Added input parsing for day 3.

---
+++ aoc2019.Puzzles.Test/Day03Test.cs
@@ -0,0 +1,31 @@
+﻿using aoc2019.Puzzles.Solutions;
+using NUnit.Framework;
+using System.Threading.Tasks;
+
+namespace aoc2019.Puzzles.Test
+{
+    public sealed class Day03Test : TestBase<Day03>
+    {
+//        [Test]
+//        public async Task Part1()
+//        {
+//            var input = @"R8,U5,L5,D3
+//U7,R6,D4,L4";
+//            Assert.That(await Solution.Part1Async(input), Is.EqualTo("6"));
+
+//            input = @"R75,D30,R83,U83,L12,D49,R71,U7,L72
+//U62,R66,U55,R34,D71,R55,D58,R83";
+//            Assert.That(await Solution.Part1Async(input), Is.EqualTo("159"));
+            
+//            input = @"R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51
+//U98,R91,D20,R16,D67,R40,U7,R15,U6,R7";
+//            Assert.That(await Solution.Part1Async(input), Is.EqualTo("135"));
+//        }
+
+//        [Test]
+//        public async Task Part2()
+//        {
+//            Assert.That(await Solution.Part2Async(""), Is.Not.Null);
+//        }
+    }
+}
---
+++ aoc2019.Puzzles/Solutions/Day03.cs
@@ -0,0 +1,60 @@
+﻿using aoc2019.Puzzles.Core;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace aoc2019.Puzzles.Solutions
+{
+    [Puzzle("Crossed Wires")]
+    public sealed class Day03 : SolutionBase
+    {
+        public override string Part1(string input)
+        {
+            var wires = ParseWires(input);
+
+            throw new NotImplementedException();
+        }
+
+        public override string Part2(string input)
+        {
+            throw new NotImplementedException();
+        }
+
+        private Point? GetIntersection(ReadOnlySpan<Point> sectionA, ReadOnlySpan<Point> sectionB)
+        {
+
+            return null;
+        }
+
+        private IReadOnlyList<Point[]> ParseWires(string input)
+        {
+            var regex = new Regex(@"(?'direction'[A-Z])(?'distance'[0-9]+)");
+            var lines = GetLines(input);
+            var wires = new List<List<Point>>();
+            foreach (var line in lines)
+            {
+                var pos = new Point(0, 0);
+                var wire = new List<Point> { pos };
+                foreach (var match in regex.Matches(line).OfType<Match>())
+                {
+                    var direction = match.Groups["direction"].Value.First();
+                    var distance = Convert.ToInt32(match.Groups["distance"].Value);
+                    switch (direction)
+                    {
+                        case 'U': pos = new Point(pos.X, pos.Y + distance); break;
+                        case 'R': pos = new Point(pos.X + distance, pos.Y); break;
+                        case 'D': pos = new Point(pos.X, pos.Y - distance); break;
+                        case 'L': pos = new Point(pos.X - distance, pos.Y); break;
+                    }
+                    wire.Add(pos);
+                }
+                wires.Add(wire);
+            }
+
+            return wires.Select(x => x.ToArray()).ToList();
+        }
+    }
+}
