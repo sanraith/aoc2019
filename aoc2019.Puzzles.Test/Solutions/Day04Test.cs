efb29af4c6ba40540c07c872dbd364b231f16551
sanraith <zsjaks@gmail.com>
2019-12-04 08:56:15 +0100

    Added brute force solution for day 4.

---
+++ aoc2019.Puzzles.Test/Solutions/Day04Test.cs
@@ -0,0 +1,23 @@
+﻿using aoc2019.Puzzles.Solutions;
+using NUnit.Framework;
+using System.Threading.Tasks;
+
+namespace aoc2019.Puzzles.Test.Solutions
+{
+    public sealed class Day04Test : TestBase<Day04>
+    {
+        [Test]
+        public async Task Part1()
+        {
+            Assert.That(await Solution.Part1Async(myInput), Is.EqualTo("925"));
+        }
+
+        [Test]
+        public async Task Part2()
+        {
+            Assert.That(await Solution.Part2Async(myInput), Is.EqualTo("607"));
+        }
+
+        private readonly string myInput = "271973-785961";
+    }
+}
---
+++ docs/source/Day04.cs
@@ -0,0 +1,56 @@
+﻿using aoc2019.Puzzles.Core;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace aoc2019.Puzzles.Solutions
+{
+    [Puzzle("Secure Container")]
+    public sealed class Day04 : SolutionBase
+    {
+        public override async Task<string> Part1Async(string input)
+        {
+            var (min, max) = ParseRange(input);
+            var count = await CountPasswords(min, max, hasExactlyTwoAdjacentDigits: false);
+
+            return count.ToString();
+        }
+
+        public override async Task<string> Part2Async(string input)
+        {
+            var (min, max) = ParseRange(input);
+            var count = await CountPasswords(min, max, hasExactlyTwoAdjacentDigits: true);
+
+            return count.ToString();
+        }
+
+        private async Task<int> CountPasswords(int min, int max, bool hasExactlyTwoAdjacentDigits)
+        {
+            var count = 0;
+            for (var number = min; number <= max; number++)
+            {
+                if (IsUpdateProgressNeeded()) { await UpdateProgressAsync(number - min, max - min); }
+
+                var digits = number.ToString();
+                var neverDecreases = true;
+                for (var i = 1; i < digits.Length; i++)
+                {
+                    if (digits[i] < digits[i - 1]) { neverDecreases = false; break; }
+                }
+
+                if (neverDecreases && digits.GroupBy(x => x).Any(x => hasExactlyTwoAdjacentDigits ? x.Count() == 2 : x.Count() > 1))
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+
+        private static (int Min, int Max) ParseRange(string input)
+        {
+            var parts = GetLines(input).First().Split('-').Select(x => Convert.ToInt32(x)).ToList();
+            return (parts[0], parts[1]);
+        }
+    }
+}
