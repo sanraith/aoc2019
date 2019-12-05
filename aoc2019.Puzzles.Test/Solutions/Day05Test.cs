9c2f906f9d1ccd285fa19d5f78afe1248af8ee19
Soma Zsják <zsjaks@gmail.com>
2019-12-05 21:31:20 +0100

    Added day 5 solution.

---
+++ aoc2019.Puzzles.Test/Solutions/Day05Test.cs
@@ -0,0 +1,59 @@
+﻿using aoc2019.Puzzles.Solutions;
+using NUnit.Framework;
+using System.Threading.Tasks;
+
+namespace aoc2019.Puzzles.Test.Solutions
+{
+    public sealed class Day05Test : TestBase<Day05>
+    {
+        [Test]
+        public async Task Part1_InputOutput()
+        {
+            var input = "3,0,4,0,99";
+            Assert.That(await Solution.Part1Async(input), Is.EqualTo("1"));
+        }
+
+        [Test]
+        public async Task Part1_ParameterModes()
+        {
+            var input = "1002,7,3,7,4,7,99,33";
+            Assert.That(await Solution.Part1Async(input), Is.EqualTo("99"));
+        }
+
+        [Test]
+        public async Task Part1_NegativeNumbers()
+        {
+            var input = "1101,100,-89,7,4,7,99,0";
+            Assert.That(await Solution.Part1Async(input), Is.EqualTo("11"));
+        }
+
+        [Test]
+        public async Task Part2_CompareOperators()
+        {
+            var input = "3,9,8,9,10,9,4,9,99,-1,8";
+            Assert.That(await Solution.Part2Async(input), Is.EqualTo("0"));
+            
+            input = "3,9,7,9,10,9,4,9,99,-1,8";
+            Assert.That(await Solution.Part2Async(input), Is.EqualTo("1"));
+
+            input = "3,3,1108,-1,8,3,4,3,99";
+            Assert.That(await Solution.Part2Async(input), Is.EqualTo("0"));
+
+            input = "3,3,1107,-1,8,3,4,3,99";
+            Assert.That(await Solution.Part2Async(input), Is.EqualTo("1"));
+        }
+
+        [Test]
+        public async Task Part2_JumpOperators()
+        {
+            var input = "3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9";
+            Assert.That(await Solution.Part2Async(input), Is.EqualTo("1"));
+
+            input = "3,3,1105,-1,9,1101,0,0,12,4,12,99,1";
+            Assert.That(await Solution.Part2Async(input), Is.EqualTo("1"));
+
+            input = "3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99";
+            Assert.That(await Solution.Part2Async(input), Is.EqualTo("999"));
+        }
+    }
+}
---
+++ aoc2019.Puzzles/Solutions/Day05.cs
@@ -0,0 +1,118 @@
+﻿using aoc2019.Puzzles.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace aoc2019.Puzzles.Solutions
+{
+    [Puzzle("Sunny with a Chance of Asteroids")]
+    public sealed class Day05 : SolutionBase
+    {
+        public override string Part1(string input)
+        {
+            var program = ParseProgram(input);
+            var outputStream = RunProgram(program, new[] { 1 });
+
+            return outputStream.Last().ToString();
+        }
+
+        public override string Part2(string input)
+        {
+            var program = ParseProgram(input);
+            var outputStream = RunProgram(program, new[] { 5 });
+
+            return outputStream.Last().ToString();
+        }
+
+        private static IEnumerable<int> RunProgram(int[] memory, IEnumerable<int> inputStream)
+        {
+            int[] rawParams = new int[ParameterCountsByOpCode.Values.Max()];
+            int[] parsedParams = new int[ParameterCountsByOpCode.Values.Max()];
+            var inputEnumerator = inputStream.GetEnumerator();
+
+            int pos = 0;
+            while (pos < memory.Length)
+            {
+                var (opCode, parameterModes) = ParseInstruction(memory[pos]);
+                ParseParams(memory, pos, parameterModes, ref rawParams, ref parsedParams);
+
+                switch (opCode)
+                {
+                    case 1:
+                        memory[rawParams[2]] = parsedParams[0] + parsedParams[1];
+                        break;
+                    case 2:
+                        memory[rawParams[2]] = parsedParams[0] * parsedParams[1];
+                        break;
+                    case 3:
+                        memory[rawParams[0]] = inputEnumerator.MoveNext() ? inputEnumerator.Current :
+                            throw new IndexOutOfRangeException("Not enough input values!");
+                        break;
+                    case 4:
+                        yield return parsedParams[0];
+                        break;
+                    case 5:
+                        if (parsedParams[0] != 0) { pos = parsedParams[1]; continue; }
+                        break;
+                    case 6:
+                        if (parsedParams[0] == 0) { pos = parsedParams[1]; continue; }
+                        break;
+                    case 7:
+                        memory[rawParams[2]] = parsedParams[0] < parsedParams[1] ? 1 : 0;
+                        break;
+                    case 8:
+                        memory[rawParams[2]] = parsedParams[0] == parsedParams[1] ? 1 : 0;
+                        break;
+                    case 99:
+                        yield break;
+                    default:
+                        throw new InvalidOperationException($"Unknown operator: {opCode}");
+                }
+
+                pos += parameterModes.Length + 1;
+            }
+        }
+
+        private static (int OpCode, int[] ParameterModes) ParseInstruction(int instruction)
+        {
+            var opCode = instruction % 100;
+            instruction /= 100;
+
+            var parameterCount = ParameterCountsByOpCode[opCode];
+            var parameterModes = new int[parameterCount];
+            var index = 0;
+            while (instruction > 0)
+            {
+                parameterModes[index++] = instruction % 10;
+                instruction /= 10;
+            }
+
+            return (opCode, parameterModes);
+        }
+
+        private static void ParseParams(int[] memory, int opPos, int[] parameterModes, ref int[] rawParams, ref int[] parsedParams)
+        {
+            var count = parameterModes.Length;
+            Array.Copy(memory, opPos + 1, rawParams, 0, count);
+            for (var i = 0; i < count; i++)
+            {
+                parsedParams[i] = parameterModes[i] == 0 ? memory[rawParams[i]] : rawParams[i];
+            }
+        }
+
+        private static int[] ParseProgram(string input) => GetLines(input).First().Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries).Select(x => Convert.ToInt32(x)).ToArray();
+
+        private static readonly Dictionary<int, int> ParameterCountsByOpCode = new Dictionary<int, int>
+        {
+            [1] = 3,
+            [2] = 3,
+            [3] = 1,
+            [4] = 1,
+            [5] = 2,
+            [6] = 2,
+            [7] = 3,
+            [8] = 3,
+            [99] = 0
+        };
+    }
+}
