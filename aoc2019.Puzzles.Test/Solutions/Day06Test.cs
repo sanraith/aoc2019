465ad0bef2e26d7a4a4c4c3de8d3a07d50d5507a
sanraith <zsjaks@gmail.com>
2019-12-06 10:17:15 +0100

    Added day 6 solution.

---
+++ aoc2019.Puzzles.Test/Solutions/Day06Test.cs
@@ -0,0 +1,45 @@
+﻿using aoc2019.Puzzles.Solutions;
+using NUnit.Framework;
+using System.Threading.Tasks;
+
+namespace aoc2019.Puzzles.Test.Solutions
+{
+    public sealed class Day06Test : TestBase<Day06>
+    {
+        [Test]
+        public async Task Part1()
+        {
+            var input = @"COM)B
+B)C
+C)D
+D)E
+E)F
+B)G
+G)H
+D)I
+E)J
+J)K
+K)L";
+            Assert.That(await Solution.Part1Async(input), Is.EqualTo("42"));
+        }
+
+        [Test]
+        public async Task Part2()
+        {
+            var input = @"COM)B
+B)C
+C)D
+D)E
+E)F
+B)G
+G)H
+D)I
+E)J
+J)K
+K)L
+K)YOU
+I)SAN";
+            Assert.That(await Solution.Part2Async(input), Is.EqualTo("4"));
+        }
+    }
+}
---
+++ docs/source/Day06.cs
@@ -0,0 +1,87 @@
+﻿using aoc2019.Puzzles.Core;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace aoc2019.Puzzles.Solutions
+{
+    [Puzzle("Universal Orbit Map")]
+    public sealed class Day06 : SolutionBase
+    {
+        public override string Part1(string input)
+        {
+            var planets = ParsePlanets(input);
+            var sum = planets.Values.Sum(p => p.DirectOrbitCount + p.IndirectOrbitCount);
+
+            return sum.ToString();
+        }
+
+        public override string Part2(string input)
+        {
+            var planets = ParsePlanets(input);
+            var pathFromYou = GetPathToCenterOfMass(planets[You]);
+            var pathFromSanta = GetPathToCenterOfMass(planets[Santa]);
+            var commonParent = pathFromYou.Intersect(pathFromSanta).First();
+            var transferCount = pathFromYou.IndexOf(commonParent) + pathFromSanta.IndexOf(commonParent);
+
+            return transferCount.ToString();
+        }
+
+        private List<Planet> GetPathToCenterOfMass(Planet planet)
+        {
+            var path = new List<Planet>();
+            while (planet.Orbits != null)
+            {
+                planet = planet.Orbits;
+                path.Add(planet);
+            }
+
+            return path;
+        }
+
+        private IDictionary<string, Planet> ParsePlanets(string input)
+        {
+            var planets = new ConcurrentDictionary<string, Planet>();
+            foreach (var line in GetLines(input))
+            {
+                var pair = line.Trim().Split(')');
+
+                var parentPlanet = planets.GetOrAdd(pair[0], name => new Planet(name));
+                var childPlanet = planets.GetOrAdd(pair[1], name => new Planet(name));
+                childPlanet.Orbits = parentPlanet;
+            }
+
+            return planets;
+        }
+
+        [DebuggerDisplay("{Name}")]
+        private sealed class Planet
+        {
+            public string Name { get; }
+
+            public Planet Orbits { get; set; }
+
+            public int DirectOrbitCount => Orbits == null ? 0 : 1;
+
+            public int IndirectOrbitCount
+            {
+                get
+                {
+                    if (myIndirectOrbitCount == null)
+                    {
+                        myIndirectOrbitCount = Orbits == null ? 0 : Orbits.DirectOrbitCount + Orbits.IndirectOrbitCount;
+                    }
+                    return myIndirectOrbitCount.Value;
+                }
+            }
+
+            public Planet(string name) => Name = name;
+
+            private int? myIndirectOrbitCount;
+        }
+
+        private const string You = "YOU";
+        private const string Santa = "SAN";
+    }
+}
