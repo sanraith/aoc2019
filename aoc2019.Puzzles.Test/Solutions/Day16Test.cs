253725b656dd3689327678f6026eee1957318d8f
Soma Zsják <zsjaks@gmail.com>
2019-12-16 18:45:43 +0100

    Added day 16 part 1 solution.

---
+++ aoc2019.Puzzles.Test/Solutions/Day16Test.cs
@@ -0,0 +1,35 @@
+﻿using aoc2019.Puzzles.Solutions;
+using NUnit.Framework;
+using System.Threading.Tasks;
+
+namespace aoc2019.Puzzles.Test.Solutions
+{
+    public sealed class Day16Test : TestBase<Day16>
+    {
+        [Test]
+        public async Task Part1()
+        {
+            var input = "80871224585914546619083218645595";
+            Assert.That(await Solution.Part1Async(input), Is.EqualTo("24176176"));
+
+            input = "19617804207202209144916044189917";
+            Assert.That(await Solution.Part1Async(input), Is.EqualTo("73745418"));
+
+            input = "69317163492948606335995924319873";
+            Assert.That(await Solution.Part1Async(input), Is.EqualTo("52432133"));
+        }
+
+        //[Test]
+        public async Task Part2()
+        {
+            var input = "03036732577212944063491565474664";
+            Assert.That(await Solution.Part2Async(input), Is.EqualTo("84462026"));
+
+            input = "02935109699940807407585447034323";
+            Assert.That(await Solution.Part2Async(input), Is.EqualTo("78725270"));
+
+            input = "03081770884921959731165446850517";
+            Assert.That(await Solution.Part2Async(input), Is.EqualTo("53553731"));
+        }
+    }
+}
---
+++ aoc2019.Puzzles/Solutions/Day16.cs
@@ -0,0 +1,60 @@
+﻿using aoc2019.Puzzles.Core;
+using aoc2019.Puzzles.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace aoc2019.Puzzles.Solutions
+{
+    [Puzzle("Flawed Frequency Transmission")]
+    public sealed class Day16 : SolutionBase
+    {
+        public override async Task<string> Part1Async(string input)
+        {
+            var inputSequence = GetSequence(input);
+            var resultSequence = await RunFFT(inputSequence, 100);
+
+            return string.Join(string.Empty, resultSequence.Take(8));
+        }
+
+        public override async Task<string> Part2Async(string input)
+        {
+            var inputSequence = Enumerable.Repeat(GetSequence(input), 10000).SelectMany(x => x).ToArray();
+            var offset = Convert.ToInt32(string.Join(string.Empty, inputSequence.Take(7)), 10);
+            
+            throw new NotImplementedException();
+            
+            var resultSequence = await RunFFT(inputSequence, 100);
+            return string.Join(string.Empty, resultSequence.Skip(offset).Take(8));
+        }
+
+        private async Task<int[]> RunFFT(int[] sequence, int phaseCount)
+        {
+            var pattern = new[] { 0, 1, 0, -1 };
+            var patternLength = pattern.Length;
+            var sequenceLength = sequence.Length;
+
+            var nextSequence = new int[sequenceLength];
+            for (var phaseIndex = 0; phaseIndex < phaseCount; phaseIndex++)
+            {
+                if (IsUpdateProgressNeeded()) { await UpdateProgressAsync(phaseIndex, phaseCount); }
+
+                for (var outputIndex = 0; outputIndex < sequenceLength; outputIndex++)
+                {
+                    var sum = 0;
+                    for (var i = 0; i < sequenceLength; i++)
+                    {
+                        sum += sequence[i] * pattern[(i + 1) / (outputIndex + 1) % patternLength];
+                    }
+                    nextSequence[outputIndex] = Math.Abs(sum) % 10;
+                }
+                (sequence, nextSequence) = (nextSequence, sequence);
+            }
+
+            return sequence;
+        }
+
+        private static int[] GetSequence(string input) => GetLines(input).First().Select(c => c - 48).ToArray();
+    }
+}
