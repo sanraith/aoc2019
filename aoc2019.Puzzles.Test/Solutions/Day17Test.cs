18c651809d9bcab11e07cc713dc9cefadf5918bb
Soma Zsják <zsjaks@gmail.com>
2019-12-17 08:41:51 +0100

    Added input and empty solution for day 17.

---
+++ aoc2019.Puzzles.Test/Solutions/Day17Test.cs
@@ -0,0 +1,23 @@
+﻿using aoc2019.Puzzles.Solutions;
+using NUnit.Framework;
+using System.Threading.Tasks;
+
+namespace aoc2019.Puzzles.Test.Solutions
+{
+    public sealed class Day17Test : TestBase<Day17>
+    {
+        // [Test]
+        public async Task Part1()
+        {
+            var input = @"";
+            Assert.That(await Solution.Part1Async(input), Is.EqualTo(""));
+        }
+
+        // [Test]
+        public async Task Part2()
+        {
+            var input = @"";
+            Assert.That(await Solution.Part2Async(input), Is.EqualTo(""));
+        }
+    }
+}
---
+++ aoc2019.Puzzles/Solutions/Day17.cs
@@ -0,0 +1,23 @@
+﻿using aoc2019.Puzzles.Core;
+using aoc2019.Puzzles.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace aoc2019.Puzzles.Solutions
+{
+    [Puzzle("Set and Forget")]
+    public sealed class Day17 : SolutionBase
+    {
+        public override async Task<string> Part1Async(string input)
+        { 
+            throw new NotImplementedException();
+        }
+
+        public override async Task<string> Part2Async(string input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
