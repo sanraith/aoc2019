ccd52f993ca75e6d790b80d03eb04fe550ed90ea
sanraith <zsjaks@gmail.com>
2019-12-17 09:34:30 +0100

    Added day 17 part 1 solution.

--- aoc2019.Puzzles/Solutions/Day17.cs
+++ aoc2019.Puzzles/Solutions/Day17.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using static aoc2019.Puzzles.Solutions.Day11;
 
 namespace aoc2019.Puzzles.Solutions
 {
@@ -11,12 +12,55 @@ namespace aoc2019.Puzzles.Solutions
     public sealed class Day17 : SolutionBase
     {
         public override async Task<string> Part1Async(string input)
-        { 
-            throw new NotImplementedException();
+        {
+            var intMachine = new SynchronousIntMachine(input);
+
+            var lines = new List<char[]>();
+            var line = new List<char>();
+            while (intMachine.RunUntilBlockOrComplete() != SynchronousIntMachine.ReturnCode.Completed)
+            {
+                if (IsUpdateProgressNeeded()) { await UpdateProgressAsync(); }
+
+                var c = intMachine.OutputQueue.Dequeue();
+                if (c == 10 && line.Any())
+                {
+                    lines.Add(line.ToArray());
+                    line = new List<char>();
+                }
+                else
+                {
+                    line.Add((char)c);
+                }
+            }
+
+            var width = lines.First().Length;
+            var height = lines.Count;
+            var alignmentSum = 0;
+            for (var y = 1; y < height - 1; y++)
+            {
+                for (var x = 1; x < width - 1; x++)
+                {
+                    if (lines[y][x] == '#' &&
+                        lines[y - 1][x] == '#' &&
+                        lines[y + 1][x] == '#' &&
+                        lines[y][x + 1] == '#' &&
+                        lines[y][x - 1] == '#')
+                    {
+                        alignmentSum += x * y;
+                    }
+                }
+            }
+
+            // Draw the map
+            Console.WriteLine();
+            lines.ForEach(l => Console.WriteLine(new string(l)));
+
+            return alignmentSum.ToString();
         }
 
         public override async Task<string> Part2Async(string input)
         {
+            if (IsUpdateProgressNeeded()) { await UpdateProgressAsync(); }
             throw new NotImplementedException();
         }
     }
