7561516b180192909eb9823020e0a92c5250e3b0
Soma Zsják <zsjaks@gmail.com>
2019-12-20 16:50:18 +0100

    Started working on day 20.

---
+++ aoc2019.Puzzles.Test/Solutions/Day20Test.cs
@@ -0,0 +1,41 @@
+﻿using aoc2019.Puzzles.Solutions;
+using NUnit.Framework;
+using System.Threading.Tasks;
+
+namespace aoc2019.Puzzles.Test.Solutions
+{
+    public sealed class Day20Test : TestBase<Day20>
+    {
+        [Test]
+        public async Task Part1()
+        {
+            var input = @"         A           
+         A           
+  #######.#########  
+  #######.........#  
+  #######.#######.#  
+  #######.#######.#  
+  #######.#######.#  
+  #####  B    ###.#  
+BC...##  C    ###.#  
+  ##.##       ###.#  
+  ##...DE  F  ###.#  
+  #####    G  ###.#  
+  #########.#####.#  
+DE..#######...###.#  
+  #.#########.###.#  
+FG..#########.....#  
+  ###########.#####  
+             Z       
+             Z       ";
+            Assert.That(await Solution.Part1Async(input), Is.EqualTo("23"));
+        }
+
+        [Test]
+        public async Task Part2()
+        {
+            var input = @"";
+            Assert.That(await Solution.Part2Async(input), Is.EqualTo(""));
+        }
+    }
+}
---
+++ aoc2019.Puzzles/Solutions/Day20.cs
@@ -0,0 +1,56 @@
+﻿using aoc2019.Puzzles.Core;
+using aoc2019.Puzzles.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using static aoc2019.Puzzles.Solutions.Day10;
+
+namespace aoc2019.Puzzles.Solutions
+{
+    [Puzzle("Donut Maze")]
+    public sealed class Day20 : SolutionBase
+    {
+        public override async Task<string> Part1Async(string input)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override async Task<string> Part2Async(string input)
+        {
+            throw new NotImplementedException();
+        }
+
+        private Dictionary<Point, Point> GetPortals(Dictionary<Point, char> map)
+        {
+            foreach (var (pos, c) in map)
+            {
+                if (c < 'A' || c > 'Z') { continue; }
+
+                var connection = Directions.Select(d => (Point?)(pos + d)).FirstOrDefault(p => map[p.Value] == '.');
+                if (connection == null) { continue; }
+
+                var otherPos = Directions.Select(d => pos + d).FirstOrDefault(p => map[p] >= 'A' && map[p] <= 'Z');
+            }
+            return null;
+        }
+
+        private Dictionary<Point, char> ParseMap(string input)
+        {
+            var lines = GetLines(input);
+            var map = new Dictionary<Point, char>();
+            foreach (var (line, y) in lines.WithIndex())
+            {
+                foreach (var (c, x) in line.WithIndex())
+                {
+                    if (c == ' ') { continue; }
+                    map[new Point(x, y)] = c;
+                }
+            }
+
+            return map;
+        }
+
+        private static readonly Point[] Directions = new[] { new Point(0, -1), new Point(1, 0), new Point(0, 1), new Point(-1, 0) };
+    }
+}
